917a990842a8436c5cea201c861241f453b14032
Rita Laezza <rita.laezza@gmail.com>
2019-11-19 22:34:12 +0100

    Fixed typo.

--- gym_agx/envs/agx_env.py
+++ gym_agx/envs/agx_env.py
@@ -1,6 +1,6 @@
 from collections import OrderedDict
 import os
-import sys
+
 
 from gym import error, spaces
 from gym.utils import seeding
@@ -40,111 +40,56 @@ def convert_observation_to_space(observation):
     return space
 
 
-class AgxEnv(gym.GoalEnv):
-    """Superclass for all AGX Dynamics environments. Initializes AGX, loads scene
+class AgxEnv(gym.Env):
+    """
+    Superclass for all AGX Dynamics environments. Initializes AGX, loads scene
     from file and builds it.
     """
 
-    def __init__(self, scene_path, n_substeps):
-        if (not scene_path.startswith("/")):
-             scene_fullpath = os.path.join(os.path.dirname(__file__), "assets", scene_path)
-        if not path.exists(scene_fullpath):
-            raise IOError("File %s does not exist" % self.scene_path)
-        self.scene_path = scene_fullpath
-        self.n_substeps = n_substeps
+    def __init__(self, scene_path, frame_skip):
+        self.scene_path = scene_path
+        self.frame_skip = frame_skip
 
         if agxPython.getContext() is None:
             init = agx.AutoInit()
 
-        # TODO: Understand these steps:
+        if scene_path.startswith("/"):
+            fullpath = self.scene_path
+        else:
+            fullpath = os.path.join(os.path.dirname(__file__), "assets", self.scene_path)
+        if not path.exists(fullpath):
+            raise IOError("File %s does not exist" % fullpath)
+        self.scene = self._load_scene_from_file(fullpath)
+
         self.app = agxOSG.ExampleApplication()
         self.argParser = agxIO.ArgumentParser([sys.executable] + sys.argv)
-        self.app.addScene(self.argParser.getArgumentName(1), "_build_scene", ord('1'), True)
-
-        if self.app.init(self.argParser):
-            self.app.run()
-        else:
-            print("An error occurred while initializing ExampleApplication.")
+        self.app.addScene(argParser.getArgumentName(1), "_build_scene", ord('1'), True)
 
         self.metadata = {
-            'render.modes': ['osg', 'debug'],
+            'render.modes': ['human', 'rgb_array', 'depth_array'],
             'video.frames_per_second': int(np.round(1.0 / self.dt))
         }
 
+        #self.data = self.sim.data
+        #self.init_qpos = self.sim.data.qpos.ravel().copy()
+        self.init_qvel = self.sim.data.qvel.ravel().copy()
+
+        self._set_action_space()
+
+        action = self.action_space.sample()
+        observation, _reward, done, _info = self.step(action)
+        assert not done
+
+        self._set_observation_space(observation)
+
         self.seed()
-        #self._env_setup(initial_qpos=initial_qpos)
-        #self.initial_state = copy.deepcopy(self.sim.get_state())
 
-        # TODO: Def
-        self.goal = self._sample_goal()
-        obs = self._get_obs()
-        self.action_space = spaces.Box(-1., 1., shape=(n_actions,), dtype='float32')
-        self.observation_space = spaces.Dict(dict(
-            desired_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
-            achieved_goal=spaces.Box(-np.inf, np.inf, shape=obs['achieved_goal'].shape, dtype='float32'),
-            observation=spaces.Box(-np.inf, np.inf, shape=obs['observation'].shape, dtype='float32'),
-        ))
-
-    @property
-    def dt(self):
-        #return self.model.opt.timestep * self.n_substeps
-        return self.sim.getTimeStep() * self.n_substeps
-
-
-    # Env methods
-    # ----------------------------
-
-    def seed(self, seed=None):
-        self.np_random, seed = seeding.np_random(seed)
-        return [seed]
-
-    def step(self, action):
-        action = np.clip(action, self.action_space.low, self.action_space.high)
-        self._set_action(action)
-        self.sim.stepForward()
-        self._step_callback()
-        obs = self._get_obs()
-
-        done = False
-        info = {
-            'is_success': self._is_success(obs['achieved_goal'], self.goal),
-        }
-        reward = self.compute_reward(obs['achieved_goal'], self.goal, info)
-        return obs, reward, done, info
-
-
-    def reset(self):
-        print("Resetting...")
-        #TODO: How to reset a simulation in AGX Dynamics?
-        #return obs
-
-    def close(self):
-        if self.app is not None:
-            # self.viewer.finish()
-            self.app = None
-            agx.shutdown();
-
-    def render(self, mode='osg'):
-        self._init_camera(self.app)
-        if mode == 'osg':
-            self.app.setEnableOSGRenderer(True)
-        elif mode == 'debug':
-            self.app.setEnableDebugRenderer(True)
-
-        #TODO: What else can be moved into this function?
-
-
-    # AGX Dynamics methods
-    #-----------------------------
-
-    def _init_camera(self, eye=DEFAULT_EYE,center=DEFAULT_CENTER,up=DEFAULT_UP):
-        """Load scene from file. Read all the objects in the file and add to
-        the scene (not the simulation!) (and the scenegraph root)
-        """
+    def init_camera(self, eye=DEFAULT_EYE,center=DEFAULT_CENTER,up=DEFAULT_UP):
         self.app.setCameraHome(eye, center, up)
 
     def _load_scene_from_file(self, file_path):
-        """Load scene from file. Read all the objects in the file and add to
+        """
+        Load scene from file. Read all the objects in the file and add to
         the scene (not the simulation!) (and the scenegraph root)
         """
         scene = agxSDK.Assembly()  # Create a new empty Assembly
@@ -154,67 +99,103 @@ class AgxEnv(gym.GoalEnv):
 
         return scene
 
-    def _build_scene(self):
-        print("Building scene.")
+    def _build_scene():
         # sim - A pointer to an instance of a agxSDK::Simulation
         # app - A pointer to an instance of a agxOSG::ExampleApplication
         # root - A pointer to an instance of agxOSG::Group
         self.sim = agxPython.getContext().environment.getSimulation()
-        slef.app = agxPython.getContext().environment.getApplication()
+        self.app = agxPython.getContext().environment.getApplication()
         self.root = agxPython.getContext().environment.getSceneRoot()
 
-        scene = self._load_scene_from_file(self.scene_path)
-        self.sim.add(scene)
+        self.sim.add(self.scene)
 
-    def _reset_scene(self):
-        """Resets the scene.
-        Implement this in each subclass.
-        """
-        raise NotImplementedError()
+        init_camera()
 
-    # Extension methods
+    def _set_action_space(self):
+        bounds = self.model.actuator_ctrlrange.copy()
+        low, high = bounds.T
+        self.action_space = spaces.Box(low=low, high=high, dtype=np.float32)
+        return self.action_space
+
+    def _set_observation_space(self, observation):
+        self.observation_space = convert_observation_to_space(observation)
+        return self.observation_space
+
+    def seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    # methods to override:
     # ----------------------------
 
-    def _get_obs(self):
-        """Returns the observation.
+    def reset_scene(self):
         """
-        raise NotImplementedError()
-
-    def _set_action(self, action):
-        """Applies the given action to the simulation.
+        Resets the scene.
+        Implement this in each subclass.
         """
-        raise NotImplementedError()
+        raise NotImplementedError
 
-    def _is_success(self, achieved_goal, desired_goal):
-        """Indicates whether or not the achieved goal successfully achieved the desired goal.
+    def viewer_setup(self):
         """
-        raise NotImplementedError()
-
-    def _sample_goal(self):
-        """Samples a new goal and returns it.
-        """
-        raise NotImplementedError()
-
-    def _env_setup(self, initial_qpos):
-        """Initial configuration of the environment. Can be used to configure initial state
-        and extract information from the simulation.
+        This method is called when the viewer is initialized.
+        Optionally implement this method, if you need to tinker with camera position
+        and so forth.
         """
         pass
 
-    def _viewer_setup(self):
-        """Initial configuration of the viewer. Can be used to set the camera position,
-        for example.
-        """
-        pass
+    # -----------------------------
 
-    def _render_callback(self):
-        """A custom callback that is called before rendering. Can be used
-        to implement custom visualizations.
-        """
-        pass
+    def reset(self):
+        self.sim.reset()
+        ob = self.reset_model()
+        return ob
 
-    def _step_callback(self):
-        """A custom callback that is called after stepping the simulation. Can be used
-        to enforce additional constraints on the simulation state.
-        """
-        pass
+    def set_state(self, qpos, qvel):
+        assert qpos.shape == (self.model.nq,) and qvel.shape == (self.model.nv,)
+        old_state = self.sim.get_state()
+        new_state = mujoco_py.MjSimState(old_state.time, qpos, qvel,
+                                         old_state.act, old_state.udd_state)
+        self.sim.set_state(new_state)
+        self.sim.forward()
+
+    @property
+    def dt(self):
+        #return self.model.opt.timestep * self.frame_skip
+        return self.sim.getTimeStep() * self.frame_skip
+
+    def do_simulation(self, ctrl, n_frames):
+        # Not sure what this does, it seems to be a way of giving a control sequence
+        self.sim.data.ctrl[:] = ctrl
+        for _ in range(n_frames):
+            self.sim.stepForward()
+
+    def render(self,
+               mode='osg',
+               camera_id=None,
+               camera_name=None):
+        if mode == 'osg':
+            self.app.setEnableOSGRenderer(True)
+        elif mode == 'debug':
+            self.app.setEnableDebugRenderer(True)
+
+        # Call the init method of ExampleApplication
+        # It will setup the viewer, windows etc.
+        if self.app.init(self.argParser):
+            self.app.run()
+        else:
+            print("An error occurred while initializing ExampleApplication.")
+
+    def close(self):
+        if self.viewer is not None:
+            # self.viewer.finish()
+            self.viewer = None
+            self._viewers = {}
+
+    def get_body_com(self, body_name):
+        return self.data.get_body_xpos(body_name)
+
+    def state_vector(self):
+        return np.concatenate([
+            self.sim.data.qpos.flat,
+            self.sim.data.qvel.flat
+        ])
