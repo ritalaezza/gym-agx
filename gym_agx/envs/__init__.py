a6076e41dfc115e86e1a6b256042813265e7597a
Rita Laezza <rita.laezza@gmail.com>
2019-11-29 22:44:35 +0100

    Updated test script to take a few steps in environment.

--- gym_agx/envs/__init__.py
+++ gym_agx/envs/__init__.py
@@ -1,2 +1,3 @@
 from gym_agx.envs.wire_env import WireEnv
 from gym_agx.envs.wire.bend_wire_env import BendWireEnv
+from gym_agx.utils.agx_utils import get_state
--- gym_agx/tests/test.py
+++ gym_agx/tests/test.py
@@ -2,3 +2,8 @@ import gym
 from gym_agx import envs
 
 env = gym.make('BendWire-v0')
+env.reset()
+for _ in range(10):
+    # env.render()
+    env.step(env.action_space.sample())  # take a random action
+env.close()
