35ee43781a5a242808620a51cfa40017d7e003c4
Rita Laezza <rita.laezza@gmail.com>
2019-12-20 18:34:48 +0100

    Reorganized code to contain an agents folder with different RL strategies.

--- gym_agx/agents/ddpg/ddpg.py
+++ gym_agx/agents/ddpg/ddpg.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 
 import gym
@@ -68,14 +69,15 @@ def main(train=True):
     # Okay, now it's time to learn something! We visualize the training here for show, but this
     # slows down training quite a lot. You can always safely abort the training prematurely using
     # Ctrl + C.
-
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_path = os.path.join(dir_path, 'ddpg_{}_weights.h5f'.format(ENV_NAME))
     if train:
         agent.fit(env, nb_steps=10000, visualize=False, verbose=1)
 
         # After training is done, we save the final weights.
-        agent.save_weights('ddpg_{}_weights.h5f'.format(ENV_NAME), overwrite=True)
+        agent.save_weights(file_path, overwrite=True)
     else:
-        agent.load_weights('ddpg_{}_weights.h5f'.format(ENV_NAME))
+        agent.load_weights(file_path)
 
     # Finally, evaluate our algorithm for 5 episodes.
     agent.test(env, nb_episodes=5, visualize=True, nb_max_episode_steps=1000)
